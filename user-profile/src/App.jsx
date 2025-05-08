51a0c012273782d5f3d3491a0a6a4791cc867270
LekhaPolepalle <chandralekha@shiftgroup.ca>
2025-05-08 09:27:38 -0700

    feat:get_users

---
+++ user-profile/src/App.jsx
@@ -0,0 +1,63 @@
+import React,{ useEffect, useState } from 'react'
+import axios from 'axios'
+import './App.css'
+
+function App() {
+  
+  const API_URL = 'http://assessmentapi.bistrainer.com/rest';
+  const BEARER_TOKEN = 'E4B82B4D-0F17-872E-93D4CBE8A0B26982';
+
+  const [users, setUsers] = useState([])
+
+  useEffect(()=>{
+    axios.get(`${API_URL}/api/user`, {
+      headers: {
+        'Authorization': `Bearer ${BEARER_TOKEN}`
+      }
+    })
+    .then((response) => {
+      console.log(response.data.data.users)
+      setUsers(response.data.data.users)
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+  },[])
+
+
+
+  return (
+    <>
+      <div>
+        <h1 className="text-blue-300">User Profile</h1>
+        <table className="bg-white border border-gray-300 border-gray-300 shadow-md rounded-lg ">
+          <thead className="bg-gray-200">
+            <tr>
+              <th className="px-4 py-2">ID</th>
+              <th className="px-4 py-2">First Name</th>
+              <th className="px-4 py-2">Last Name</th>
+              <th className="px-4 py-2">Username</th>
+              <th className="px-4 py-2">Email</th>
+            </tr>
+          </thead>
+        </table>
+
+        <tbody className="bg-white">
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td className="border px-4 py-2">{user.id}</td>
+              <td className="border px-4 py-2">{user.firstName}</td>
+              <td className="border px-4 py-2">{user.lastName}</td>
+              <td className="border px-4 py-2">{user.username}</td>
+              <td className="border px-4 py-2">{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+
+       
+      </div>
+    </>
+  )
+}
+
+export default App
